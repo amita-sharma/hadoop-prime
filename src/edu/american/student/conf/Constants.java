7a27feb5cf8da195facff33ea6179e789edc2a01
Cam Cook <cam.cook@linux.com>
2012-12-15 02:58:33 -0500

    Removed my twitter info

--- src/edu/american/student/conf/Constants.java
+++ src/edu/american/student/conf/Constants.java
@@ -79,7 +79,7 @@ public enum Constants
 	 * You get this from https://dev.twitter.com/
 	 * See:https://github.com/Ccook/hadoop-prime/wiki/Level-5:-Expert-:-TwitterIngest
 	 */
-	TWITTER_OAUTH_CONSUMER_KEY("Ax0OLrkZeTv55jL4JMaTcg"),
+	TWITTER_OAUTH_CONSUMER_KEY("**********"),
 	
 	/*
 	 * A Twitter OAUTH Consumer Secret
@@ -87,7 +87,7 @@ public enum Constants
 	 * You get this from https://dev.twitter.com/
 	 * See:https://github.com/Ccook/hadoop-prime/wiki/Level-5:-Expert-:-TwitterIngest
 	 */
-	TWITTER_OAUTH_CONSUMER_SECRET("Myzm9mLt4hrpchUbHXBtEJZytan2oOgTUTJrPYmqBo"),
+	TWITTER_OAUTH_CONSUMER_SECRET("**********"),
 	
 	/*
 	 * A Twitter OAUTH Access Token
@@ -95,7 +95,7 @@ public enum Constants
 	 * You get this from https://dev.twitter.com/
 	 * See:https://github.com/Ccook/hadoop-prime/wiki/Level-5:-Expert-:-TwitterIngest
 	 */
-	TWITTER_OAUTH_ACCESS_TOKEN("588434671-7iAor9RjYnfKREv06HRL6xCRkN6TiDQXvabjuxP1"), 
+	TWITTER_OAUTH_ACCESS_TOKEN("*********************"), 
 	
 	/*
 	 * A Twitter OAUTH Access Token Secret
@@ -103,7 +103,7 @@ public enum Constants
 	 * You get this from https://dev.twitter.com/
 	 * See: https://github.com/Ccook/hadoop-prime/wiki/Level-5:-Expert-:-TwitterIngest
 	 */
-	TWITTER_OAUTH_ACESS_TOKEN_SECRET("1NEHHADQ8p6rj3N0aTpv9ols333v4uSEAtviiZESSuQ");
+	TWITTER_OAUTH_ACESS_TOKEN_SECRET("***********************");
 
 	private String name = "";
 
